0cbbb3810c210b6bc329c28355e90883effa4d13
mdkutub1787 <158472932+mdkutub1787@users.noreply.github.com>
2024-06-04 12:30:00 +0600

    addd

--- BankManagementSystem/src/bankmanagementsystem/BankAccount.java
+++ BankManagementSystem/src/bankmanagementsystem/BankAccount.java
@@ -5,11 +5,6 @@ package bankmanagementsystem;
 public abstract class BankAccount {
     
 
-
-  
-    
-    
-   
     public abstract void deposit(double amount);
     public abstract void withdraw(double amount);
     public abstract void caculateInterest(double amount);
